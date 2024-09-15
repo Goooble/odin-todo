9fef139fe732f2bf61879e4ec5874f38f3964c14
Goooble <[bivinbheemaiah@gmail.com]>
2024-09-16 00:35:40 +0530

    make todo object, addTodo function and deleteTodo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import "./reset.css";
 import "./styles.css";
 
+import { createTodo, deleteTodo, todoCont } from "./manTodo";
+
+createTodo("title");
+console.log(todoCont[0])
+deleteTodo(todoCont[0]);
+console.log(todoCont[0])
--- src/manTodo.js
+++ src/manTodo.js
@@ -1,4 +1,24 @@
-function Todo(title, description, date, priority, project){
+const todoCont = [];
 
-    return{title, description, date, priority, project}
+function todo(title, description = null, date = null, priority = null, project = "inbox"){
+    var state = false;
+
+    function getState(){
+        return state;
+    }
+    return{getState}
 }
+
+function createTodo(title, description, date, priority, project){
+    todoCont.unshift(todo(title, description, date, priority, project));
+}
+
+function deleteTodo(todo){
+    todoCont.forEach((item, index) => {
+        if(item === todo){
+            todoCont.splice(index, 1);
+        } 
+    })
+}
+
+export {todoCont, createTodo, deleteTodo};
